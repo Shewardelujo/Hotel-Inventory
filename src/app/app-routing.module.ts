3e5a7ed901c476b795ce743746441c4b9d9f190c
Shewardelujo <adesewa.adefalujo@bankly.ng>
2022-11-09 16:03:20 +0100

    router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RoomsAddComponent } from './rooms/rooms-add/rooms-add.component';
-import { RoomsBookingComponent } from './rooms/rooms-booking/rooms-booking.component';
-import { RoomsComponent } from './rooms/rooms.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   //the default pathMatch is 'prefix' and checks from the left to see the match
   { path: 'employee', component: EmployeeComponent },
-  { path: 'rooms', component: RoomsComponent },
-  { path: 'rooms/add', component: RoomsAddComponent },
-  //DYNAMIC ROUTE
-  { path: 'rooms/:roomId', component: RoomsBookingComponent },
+
   { path: 'login', component: LoginComponent },
 
   //WILD CARD ROUTE
---
+++ src/app/header/header.module.ts
@@ -0,0 +1,13 @@
+import { HeaderComponent } from './header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
+  //since HeaderComponent is used in another module, then you have to export it.
+})
+export class HeaderModule {}
+
+//since Header Component is reusable across different module and component,if it is just put in the app.module.ts, then it will not be available in other modules, hence why it has to be a module, standalone module, that can be reused across the project
