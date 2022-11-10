51da452aa513021e313e3b2544f71b82f24e59fb
Shewardelujo <adesewa.adefalujo@bankly.ng>
2022-11-10 13:16:59 +0100

    canActivate guard implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginGuard } from './guards/login.guard';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,15 +8,21 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   //the default pathMatch is 'prefix' and checks from the left to see the match
-  { path: 'employee', component: EmployeeComponent },
+  { path: 'employee', component: EmployeeComponent, canActivate: [LoginGuard] },
 
   { path: 'login', component: LoginComponent },
   {
     path: 'rooms',
     loadChildren: () =>
       import('./rooms/rooms.module').then((m) => m.RoomsModule),
+    canActivate: [LoginGuard],
   },
-  { path: 'booking', loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule) },
+  {
+    path: 'booking',
+    loadChildren: () =>
+      import('./booking/booking.module').then((m) => m.BookingModule),
+    canActivate: [LoginGuard],
+  },
 
   //WILD CARD ROUTE
   { path: '**', component: NotFoundComponent },
