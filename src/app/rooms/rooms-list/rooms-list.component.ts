449490c6829d3a4edbdab4001db0a510f87a4792
Shewardelujo <adesewa.adefalujo@bankly.ng>
2022-11-26 23:53:18 +0100

    Custom Pipe

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -15,14 +15,15 @@ import { RoomList } from '../rooms';
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrls: ['./rooms-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   //changeDetection onPush ensures that we aren't running change detection on this component until or unless needed, when something is pushed into it. AND first your component should not change the data internally, so the assigning or passing should come from the parent component.. ALSO, the data from the parent component should be immutable, return a new object or array,so that same concept of state management applies here. hence why we do *this.roomList = [...this.roomList, room]
   //ngOnChanges can also only be applied to components that has the @Input decorator
 })
 export class RoomsListComponent implements OnInit, OnChanges, OnDestroy {
   //@Input() makes rooms a valid html property in the above templateUrl passed into it when the parent is calling the child component
-  @Input() rooms: RoomList[] | null = [];
+  @Input() rooms: RoomList[] = [];
   @Input() title: string = '';
+  @Input() price = 0;
   // @Output() makes the selectedRoom/selectRoom function a valid event property in the above templateUrl
   @Output() selectedRoom = new EventEmitter<RoomList>();
 
