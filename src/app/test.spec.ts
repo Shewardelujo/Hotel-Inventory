acf180655541437a7ff819326f1b87330941b13d
Shewardelujo <adesewa.adefalujo@bankly.ng>
2022-11-27 15:55:42 +0100

    Testing beginning

---
+++ src/app/test.spec.ts
@@ -0,0 +1,12 @@
+import { Calculator } from './testService';
+
+describe('testService', () => {
+  // it('should add 2 numbers', () => {
+  //     const service = new Calculator();
+  //     expect(service.add(2, 2).toBe(4);
+  // })
+  // it('should sub 2 numbers', () => {
+  //     const service = new Calculator();
+  //     expect(service.subtract(2, 2).toBe(0);
+  // })
+});
